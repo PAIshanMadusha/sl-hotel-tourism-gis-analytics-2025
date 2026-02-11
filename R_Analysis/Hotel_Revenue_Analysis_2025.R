41c409a642e10e2f1a002f5e08b88f380c0a93cb
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2026-02-11 10:11:27 +0530

    Install the library and load the dataset

--- R_Analysis/Hotel_Revenue_Analysis_2025.R
+++ R_Analysis/Hotel_Revenue_Analysis_2025.R
@@ -1,4 +1,24 @@
 # Project: Statistical and Business Analytics of Hotel Revenue
 # Author: PAIshanMadusha
 # Purpose: Analysis of Sri Lanka's Hotel and Tourism Sector
+# ==============================================================================
+
+# 1. CLEAR ENVIRONMENT
+rm(list = ls())
+
+# 2. INSTALL AND LOAD REQUIRED LIBRARIES
+if (!require(psych)) install.packages("psych")
+if (!require(car)) install.packages("car")
+if (!require(ggplot2)) install.packages("ggplot2")
+if (!require(corrplot)) install.packages("corrplot")
+
+library(ggplot2)
+library(psych)
+library(car)
+library(corrplot)
+
+# 3. LOAD DATASET
+hotel <- read.csv("../Data/HOTELS_2025.csv")
+
+
 
